552ccd1075a8ae4062929124857a99fa511ec61d
Alexey Kirpichnikov <alex.kirp@gmail.com>
2020-07-15 01:56:00 +0500

    Switch to POST requests

--- app/main.lisp
+++ app/main.lisp
@@ -2,14 +2,14 @@
 
 (defun main ()
   (let* ((server-url (nth 1 sb-ext:*posix-argv*))
-         (player-key (nth 2 sb-ext:*posix-argv*))
-         (url (format nil "~a?playerKey=~a" server-url player-key)))
+         (player-key (nth 2 sb-ext:*posix-argv*)))
     (format t "ServerUrl: ~a; PlayerKey: ~a~%" server-url player-key)
     (multiple-value-bind (body status)
-        (drakma:http-request url)
+        (drakma:http-request server-url :method :post :content player-key)
       (case status
         (200
-         (format t "~a~%" body)
+         (format t "Server response: ~a~%" body)
          (uiop:quit 0))
         (otherwise
-         (uiop:quit 1))))))
+         (format t "Unexpected server response:~%HTTP code: ~a~%Response body: ~a~%" status body)
+         (uiop:quit 2))))))
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/solution/run "$@"
+/solution/run "$@" || echo "run error code: $?"
